8d0c8d5d3d8e50ba487bc456df9c8c00c2b2f01d
DigitalNPY <54885078+DigitalNPY@users.noreply.github.com>
2020-05-24 16:41:11 +0200

    Upload Offre_Annulation_Offerte

---
+++ Offre-annulation/listing.js
@@ -0,0 +1,200 @@
+const npySummerProm=[
+    {
+        nom:'Maison d\'Orride',
+        type:'Hotels / Chambres d\'hôtes',
+        loc:'Ledeuix',
+        station:'Lpsm',
+        ui:'OSCH-72613',
+        pic:'maison-orride.jpg'
+
+    },
+    {
+        nom:'Flocons du soleil',
+        type:'Appartement',
+        loc:'Peyragudes',
+        station:'Peyragudes',
+        ui:'OSMB-64622',
+        pic:'placeholder.jpg'
+
+    },
+    {
+        nom:'Maison de Peyragudes',
+        type:'Appartements',
+        loc:'Peyragudes',
+        station:'Peyragudes',
+        ui:'MARK-42660',
+        pic:'placeholder.jpg'
+
+    },
+    {
+        nom:'Agence pic du midi',
+        type:'Appartements',
+        loc:'Peyragudes',
+        station:'Tourmalet',
+        ui:'ZEPH-64477',
+        pic:'placeholder.jpg'
+
+    },
+    {
+        nom:'Hostellerie de Ramonjuan',
+        type:'Hotels / Chambres d\'hôtes',
+        loc:'B. de Bigorre',
+        station:'Tourmalet',
+        ui:'AVAI-39677',
+        pic:'domaine-ramonjuan.jpg'
+
+    },
+    {
+        nom:'Mer & Golf Pic du Midi',
+        type:'Résidences de tourisme',
+        loc:'La Mongie',
+        station:'Tourmalet',
+        ui:'AVAI-43796',
+        pic:'meretgolf-pic.jpg'
+
+    },
+    {
+        nom:'Mer & Golf La Mongie',
+        type:'Résidences de tourisme',
+        loc:'La Mongie',
+        station:'Tourmalet',
+        ui:'AVAI-43794',
+        pic:'meretgolf-tourmalet.jpg'
+
+    },
+    {
+        nom:'Résidence Richelieu',
+        type:'Appartements',
+        loc:'Peyragudes',
+        station:'lpsm',
+        ui:'OSMB-35536',
+        pic:'placeholder.jpg'
+
+    },
+    {
+        nom:'Résidence Gascogne',
+        type:'Appartements',
+        loc:'Peyragudes',
+        station:'lpsm',
+        ui:'OSMB-108746',
+        pic:'placeholder.jpg'
+
+    },
+    {
+        nom:'Le Bois Joli',
+        type:'Hotels / Chambres d\'hôtes',
+        loc:'Cauterets',
+        station:'Cauterets',
+        ui:'HRIT-35250',
+        pic:'hotel-boisjoli.jpg'
+
+    },
+    {
+        nom:'Carré Py',
+        type:'Hotels / Chambres d\'hôtes',
+        loc:'B. de Bigorre',
+        station:'Tourmalet',
+        ui:'AVAI-47066',
+        pic:'hotel-carrepy.jpg'
+
+    },
+    {
+        nom:'La Maisonnée',
+        type:'Hotels / Chambres d\'hôtes',
+        loc:'Sazos',
+        station:'Luz-Ardiden',
+        ui:'OSCH-48058',
+        pic:'placeholder.jpg'
+
+    },
+    {
+        nom:'Ecovillage Vacance Soleil du Pibeste',
+        type:'Campings',
+        loc:'Agos Vidalos',
+        station:'Luz',
+        ui:'CVERT-36125',
+        pic:'camping-pibeste.jpg'
+
+    },
+    {
+        nom:'Les 4 Ours',
+        type:'Hotels / Chambres d\'hôtes',
+        loc:'Piau Engaly',
+        station:'Piau',
+        ui:'NOVH-47375',
+        pic:'hotel-4ours.jpg'
+
+    },
+    {
+        nom:'Auberge de l\'étable',
+        type:'Hotels / Chambres d\'hôtes',
+        loc:'Montory',
+        station:'Lpsm',
+        ui:'HRIT-29997',
+        pic:'auberge-etable.jpg'
+
+    },
+    {
+        nom:'Selection Agence Barroso',
+        type:'Hotels / Chambres d\'hôtes',
+        loc:'Gourette',
+        station:'Gourette',
+        ui:'HRES-36724',
+        pic:'placeholder.jpg'
+
+    },
+    {
+        nom:'Les Adrets',
+        type:'Résidences de tourisme',
+        loc:'Peyragudes',
+        station:'Peyragudes',
+        ui:'OSMB-43060',
+        pic:'placeholder.jpg'
+
+    },
+    {
+        nom:'Appartement Toit',
+        type:'Résidences de tourisme',
+        loc:'Peyragudes',
+        station:'Peyragudes',
+        ui:'OSMB-109565',
+        pic:'placeholder.jpg'
+
+    },
+    {
+        nom:'Sélection Page Immobilier',
+        type:'Appartement',
+        loc:'Cauterets',
+        station:'Cauterets',
+        ui:'OSMB-109565',
+        pic:'placeholder.jpg'
+
+    },
+    {
+        nom:'Sélection Loueurs Particuliers',
+        type:'Appartement',
+        loc:'Peyragudes',
+        station:'lpsm',
+        ui:'CA-OPROM-43702-1334',
+        pic:'placeholder.jpg'
+
+    },
+    {
+        nom:'Appartement #',
+        type:'Appartement',
+        loc:'Peyragudes',
+        station:'lpsm',
+        ui:'CA-OPROM-43702-1334',
+        pic:'placeholder.jpg'
+
+    },
+
+
+
+
+
+
+    
+
+
+];
---
+++ Offre-annulation/offre_annulation_gratuite.js
@@ -0,0 +1,123 @@
+let npyIdIntegration = "1331";
+let npyIdPanier = "VRpVVVU";
+let npyUi = "OSCH-72613";
+window.langue = "fr";
+
+
+
+
+
+
+
+
+const resortButtons = Array.from(document.querySelectorAll('.durationSelector'));
+const npyWrapper = document.querySelector('.promo');
+const npyWidgetWrapper = document.querySelector('.widget_resa');
+
+//Initialisation
+filterItemsByResort('all');
+
+function filterItemsByResort(choix){
+        npyWrapper.innerHTML='';
+          for (var i = 0; i < npySummerProm.length; i++) {
+
+            if((npySummerProm[i].station)===choix || choix==='all'){
+            let npyUrl = npySummerProm[i].ui;
+            let nom = npySummerProm[i].nom;
+            let npyBackground = npySummerProm[i].pic;
+            let npyType = npySummerProm[i].type;
+            let npyLoc = npySummerProm[i].loc;
+            let npyStation =npySummerProm[i].station;
+      
+            
+
+            let item =`<div class="cont_grid4 lgmt" data-resort="${npyStation}" data-img="${npyBackground}" data-lgmt="${npyUrl}" data-titre="${nom}" data-type="${npyType}" data-loc="${npyLoc}"><div class="cont_grid_item"><div class="cont_grid_img"><img class="npyImage" /></div><div class="cont_grid_text"><h3 class="cont_title cont_title3">${nom}</h3></div></div></div>`;
+            let textnode = document.createElement('div');
+            textnode.innerHTML = item;  
+            npyWrapper.appendChild(textnode);
+            }
+
+            else{}
+         } 
+
+         let createdItems = document.querySelectorAll('.lgmt');
+         let createdImages= Array.from(document.querySelectorAll('.npyImage'));
+         let itemLocs =Array.from(document.querySelectorAll('.cont_grid_text'));
+
+         for (var i = 0; i < createdItems.length; i++) {
+            let type = createdItems[i].dataset.type;
+            let loc = createdItems[i].dataset.loc;
+            let image = createdItems[i].dataset.img;
+            createdImages[i].src= 'img/'+image;
+            
+            let itemLocsLieu = `<div class="cont_grid_baseline lieu"><img src="https://www.n-py.com/sites/n-py/files/commons/0_ICONES/gps.png"/><b>${loc}</b></div>`;
+            let itemLocsType =`<div class="cont_grid_baseline type"><img alt="" src="https://www.n-py.com/sites/n-py/files/commons/0_ICONES/gps.png" /><b>${type}</b></div>`;
+            let itemLocsNodeLieu = document.createElement('div');
+            let itemLocsNodeType = document.createElement('div');
+            itemLocsNodeLieu.innerHTML = itemLocsLieu;
+            itemLocsNodeType.innerHTML= itemLocsType;
+            itemLocs[i].appendChild(itemLocsNodeLieu);
+            itemLocs[i].appendChild(itemLocsNodeType);
+            
+
+         }
+
+
+
+         
+
+
+         createdItems.forEach(element => element.addEventListener("click",function(e){
+            npyWidgetWrapper.innerHTML='';
+            let npyUi = this.dataset.lgmt;
+            let res = npyUi.substring(0, 4);
+            console.log(res);
+            if(res==='OSMB'){
+
+            
+            }
+            else{
+              let npyNom =this.dataset.titre;
+            let widgetHtml = `<h2 class="home_title">${npyNom}</b></h2><div id="widget-produit-${npyUi}" ></div>`;
+            let npyWidget = document.createElement('div');
+            npyWidget.innerHTML = widgetHtml;
+            npyWidgetWrapper.appendChild(npyWidget);
+            let widgetProduit = AllianceReseaux.Widget.Instance( "Produit", { idPanier:npyIdPanier, idIntegration:npyIdIntegration, langue:"fr", ui:npyUi } );
+            widgetProduit.Initialise();
+            scrollToElement('widget_resa');
+            }
+         }));
+
+
+}
+
+
+
+
+
+
+
+
+ function scrollToElement(element){
+    window.scroll({
+              behavior: 'smooth',
+              left: 0,
+              // top gets the distance from the top of the page of our target element
+              top: document.getElementById(element).offsetTop
+            });
+};
+
+
+//Definition Duree
+resortButtons.forEach(element => element.addEventListener('click', function(e){
+let chosenResort= this.dataset.resort;
+for (var i = 0; i < resortButtons.length; i++) {
+    resortButtons[i].classList.remove('active');
+}
+this.classList.add('active');
+filterItemsByResort(chosenResort);
+
+//document.querySelector('iframe').src=`https://www.n-py.com/fr/${chosenResort}`;
+
+
+}));
